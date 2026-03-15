b4a0b053c7f2ee256f57182a68e8c9c7dae4b18f
lislisowni2 <rafalkozikowski735@gmail.com>
2026-03-15 19:48:47 +0100

    Add accept_keyword from RelEng, merge two scripts to one (prepare-*)

--- scripts/prepare-stages.sh
+++ scripts/prepare-stages.sh
@@ -2,9 +2,17 @@
 
 ROOT_PROJECT=$(cd "$(dirname $BASH_SOURCES[0]})" && pwd)
 CATALYST_TMP_DIR="/var/tmp/catalyst"
+CATALYST_CONF_DIR="$CATALYST_TMP_DIR/config"
 CATALYST_SCRIPTS_DIR="$CATALYST_TMP_DIR/scripts"
 CATALYST_ROOT_OVERLAY="$CATALYST_TMP_DIR/root_overlay"
 CATALYST_KERNEL_CONF="$CATALYST_TMP_DIR/kernel_config"
+
+echo ">>> Preparing stage1 environment"
+
+echo ">> Copying portage configuration to catalyst temporary directory"
+rm -rf "$CATALYST_CONF_DIR"
+mkdir -p "$CATALYST_CONF_DIR"
+cp -r "$ROOT_PROJECT/releases/." "$CATALYST_CONF_DIR/lentoo/"
 
 echo ">>> Preparing stage2 environment"
 
