c5c415f149957a18dfa73eb88ba009d5c22f9819
lislisowni2 <rafalkozikowski735@gmail.com>
2026-03-15 11:02:27 +0100

    Update prepare-stage1.sh

---
+++ scripts/prepare-stage1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT_PROJECT=$(cd "$(dirname $BASH_SOURCES[0]})" && pwd)
+CATALYST_TMP_DIR="/var/tmp/catalyst"
+CATALYST_CONF_DIR="$CATALYST_CONF_DIR/lentoo-config"
+
+echo ">>> Preparing stage1 environment"
+
+rm -rf "$CATALYST_CONF_DIR"
+mkdir -p "$CATALYST_CONF_DIR"
+cp -r "$ROOT_PROJECT/releases/portage/." "$CATALYST_CONF_DIR/"
